948164d484e883ee38a5c638b192d92b98fb9aa5
Angelo Carvalho <angelosuporteti@gmail.com>
2021-05-09 17:18:41 -0300

    DonutChart integration - used criterion of shekel of life

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests'
@@ -9,19 +10,21 @@ type ChartData = {
 }
 const DonutChart = () => {
 
-    let chartData : ChartData = { labels: [], series: []};
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] })
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries =data.map(x => x.sum);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            chartData  ={labels: myLabels, series: mySeries};
-            console.log(response.data);
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                setChartData({ labels: myLabels, series: mySeries });
 
-        });
+            });
+
+    }, []);
 
     //const mockData = {
     //     series: [477138, 499928, 444867, 220426, 473088],
@@ -34,14 +37,14 @@ const DonutChart = () => {
     }
 
     return (
-        <Chart 
-            options={{...options, labels: chartData.labels}}
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
         />
     );
-    
+
 }
 
 export default DonutChart;

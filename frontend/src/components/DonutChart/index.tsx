79d6160767261190761e0aff2535b033ef62306c
Angelo Carvalho <angelosuporteti@gmail.com>
2021-05-09 17:01:45 -0300

    Correction in First request - Donuts chart keep not showing

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -27,7 +27,20 @@ const DonutChart = () => {
     //     series: [477138, 499928, 444867, 220426, 473088],
     //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     //}
-    
+    const options = {
+        legend: {
+            show: true
+        }
+    }
+
+    return (
+        <Chart 
+            options={{...options, labels: chartData.labels}}
+            series={chartData.series}
+            type="donut"
+            height="240"
+        />
+    );
     
 }
 
